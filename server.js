c19ed49978f96fdf912e9f29be8ce058683e2a84
Shivang <rawatofficial2021@gmail.com>
2025-05-04 13:36:38 +0530

    forth commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./api/userRoutes');
+const userRoutes = require('./userRoutes');
 require('dotenv').config();
 
 const app = express();
